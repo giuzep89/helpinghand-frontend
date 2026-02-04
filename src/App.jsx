3548589bcde945f087db903fc80e24ec15e84f5b
giuzep89 <g.rinaldimusic@gmail.com>
2026-02-04 15:09:09 +0100

    Added empty files for the individual pages and set up routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Routes, Route } from 'react-router-dom';
+import './App.css';
+import LoginRegistration from './pages/login-registration/LoginRegistration.jsx';
+import Home from './pages/home/Home.jsx';
+import Friends from './pages/friends/Friends.jsx';
+import Messages from './pages/messages/Messages.jsx';
+import Profile from './pages/profile/Profile.jsx';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {/* TODO: Add Sidebar here with condition to hide on login page */}
+      <Routes>
+        <Route path="/login" element={<LoginRegistration />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/friends" element={<Friends />} />
+        <Route path="/messages" element={<Messages />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
